d8ce565da8d6a25908937b1282f615a65cd0c230
Nandini9390 <tsnandini5@gmail.com>
2025-07-13 15:38:20 +0530

    Add files via upload

---
+++ jinja/myproject/main/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+   
+     path('displaygreen/', views.displaygreen, name='displaygreen'),
+    
+]
---
+++ jinja/myproject/main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+def displaygreen(request):
+    stu=[{'name':'shiva','marks':90},{'name':'navmohan','marks':85},
+         {'name':'shravani','marks':88},{'name':'salma','marks':91}]
+
+    return render(request,'green.html',{'data': stu})
+
